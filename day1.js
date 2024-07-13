364168795d89f7ee26711b24ef5491204839b239
Talha shiekh <tk.shiekh4567@gmail.com>
2024-07-12 23:37:10 -0700

    Completed First Day

---
+++ day1.js
@@ -0,0 +1,32 @@
+//                                 Activity 1
+// Task1
+var a = 1;
+console.log(a); // 1
+
+// Task2
+let str = "Hello World";
+console.log(str); // Hello World
+
+//                                 Activity 2
+// Task3
+const b = true;
+console.log(b); // true
+
+//                                 Activity 3
+// Task4
+let num = 3,string="How are you",bool = true,obj={name:'Talha shiekh'},arr=[1,2,3]; 
+
+console.log(typeof num,typeof string,typeof bool,typeof obj,Array.isArray(arr)); // number string boolean object true
+
+//                                 Activity 4
+let var1 = "First Value";
+
+console.log(var1); // First Value
+
+var1 = "New Value";
+
+console.log(var1) // New Value
+
+//                                 Activity 5
+const var2 = "Constant Value";
+var2 = "New Constant Value"; //!  Assignment to constant variable
