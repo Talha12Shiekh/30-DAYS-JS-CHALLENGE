bc5a3a80ea22df9b84739550cc6cb74fd41533f4
Talha shiekh <tk.shiekh4567@gmail.com>
2024-07-16 00:29:21 -0700

    completed 4th day

---
+++ day4.js
@@ -0,0 +1,83 @@
+//                               Activity 1
+
+// Task 1
+
+
+// for(let i = 0;i<=10;i++){
+//     console.log(i);
+// }
+
+// Task 2
+// let table = 5;
+// for(let i = 1;i<=10;i++){
+//     console.log(`${table} X ${i} = ${table*i}`)
+// }
+
+//                               Activity 2
+
+// Task 3
+// let sum = 0,i=0;
+// while(i<=10){
+//     sum += i;
+//     i++;
+// }
+// console.log(sum);
+
+// Task 4
+// let j = 10;
+// while(j>=1){
+//     console.log(j);
+//     j--
+// }
+
+//                               Activity 3
+
+// Task 5
+// let k = 0;
+// do{
+//     console.log(k);
+//     k++
+// }while(k<=5)
+
+
+// Task 6
+
+// function factorial(num){
+//     if (num === 0 || num === 1) return 1;
+//     return num * factorial(num - 1);
+// }
+
+// console.log(factorial(3))
+
+// let fact = 1;num=4;
+// do{
+//     if (num === 0 || num === 1) break;
+//     fact = fact * num;
+//     num--
+// }while(num>=0);
+
+// console.log(fact)
+
+//                               Activity 4
+
+// Task 7
+
+// for(let i=0;i<=5;i++){
+//    let str = "* ";
+//    console.log(str.repeat(i));
+// }
+
+//                               Activity 5
+
+// Task 8
+// for(let i=1;i<=10;i++){
+//     if(i===5) continue;
+//     console.log(i)
+// }
+
+// Task 9
+for(let i=1;i<=10;i++){
+    if(i===7) break;
+    console.log(i)
+}
+
