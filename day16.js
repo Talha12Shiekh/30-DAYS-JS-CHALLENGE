641bc1d847751e65d441a87cb2982d8d2ea08161
Talha shiekh <tk.shiekh4567@gmail.com>
2024-07-30 06:56:08 -0700

    pending day 16

---
+++ day16.js
@@ -0,0 +1,28 @@
+// function factorial(n){
+//     if(n==1 || n==0) return 1;
+
+//     return n * factorial(n - 1);
+// }
+
+// console.log(factorial(3))
+// console.log(factorial(4))
+// console.log(factorial(5))
+
+// function fabonacci(n){
+//     if(n==1 || n== 2) return 1;
+
+//    return fabonacci(n - 2) + fabonacci(n - 1);
+// }   
+
+
+// console.log(fabonacci(9))
+
+// function maximun(arr,length){
+//  if(length <= 0)return 0
+    
+//  return Math.max(arr[length - 1],arr[length - 2]);
+// }
+
+// let arr = [1,2,3,4,10,6,7]
+
+// console.log(maximun(arr,arr.length))
